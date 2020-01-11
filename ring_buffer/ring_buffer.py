164693bc2c3a7dd5311bc29b462368f094a231cf
jamesbieber <jamesbieber97@gmail.com>
2020-01-11 13:01:11 -0600

    MVP

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,15 +8,40 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        else:
+            # if an item needs to be deleted, it is the head.
+            delete = self.storage.head
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+
+            if delete == self.current:
+                self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        first = self.current  # keep track of first node so no duplicates
+        list_buffer_contents.append(first.value)
+
+        node = first  # Changing node
+        if node.next is not None:
+            node = node.next
+        else:
+            node = self.storage.head
+
+        while node is not first:
+            list_buffer_contents.append(node.value)
+            if node.next is not None:
+                node = node.next
+            else:
+                node = self.storage.head
 
         return list_buffer_contents
+
 
 # ----------------Stretch Goal-------------------
 
